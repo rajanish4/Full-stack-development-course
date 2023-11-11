801d14fe48316c553cc7df9e9c738fce4f8cbdcf
Anish Raj <rajanish4@gmail.com>
2023-11-11 15:44:15 +0100

    create production build of notes app

--- Part 3/example/index.js
+++ Part 3/example/index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 
 app.use(cors())
+app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
